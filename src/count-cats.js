03e70829f2018b13781645209f72543fb45059eb
Maxim Petrischak <petrischak.maxim@gmail.com>
2020-02-26 10:49:07 +0200

    feat: create countCats function

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,9 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(arr) {
+  if (arr.lenght === 0) return 0;
+  const cat = '^^';
+  return arr
+    .map(el => el)
+    .join()
+    .split(',')
+    .filter(string => string === cat).length;
 };
