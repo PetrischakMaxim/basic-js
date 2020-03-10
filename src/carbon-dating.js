b82dbc8c0ca7a5a6040daf43b65227c0b97ebe95
Maxim Petrischak <petrischak.maxim@gmail.com>
2020-03-10 18:26:04 +0200

    create dateSample function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,10 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  const TIME = 0.693;
+  if(typeof(sampleActivity) !== 'string' || isNaN(parseFloat(sampleActivity)) || Number(sampleActivity) <= 0 || Number(sampleActivity) > MODERN_ACTIVITY) {
+    return false;
+  }
+  return Math.ceil((Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / (TIME / HALF_LIFE_PERIOD)));
 };
