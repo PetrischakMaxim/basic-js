39981467b52c208a61ef7b944a70f3daebb419e7
Maxim Petrischak <petrischak.maxim@gmail.com>
2020-03-11 11:17:49 +0200

    feat: create getSeason function

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,26 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(str) {
+  if (!str) return 'Unable to determine the time of year!';
+  if (str.hasOwnProperty('getMonth')) throw new Error();
+  const currentMonth = str.getMonth();
+
+  switch (currentMonth) {
+    case 11:
+    case 0:
+    case 1:
+      return 'winter';
+    case 2:
+    case 3:
+    case 4:
+      return 'spring';
+    case 5:
+    case 6:
+    case 7:
+      return 'summer';
+    case 8:
+    case 9:
+    case 10:
+      return 'autumn';
+  }
+
+  return false;
 };
